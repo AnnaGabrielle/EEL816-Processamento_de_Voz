29cb243001591fe229d6278cb7602366e05c317d
Marcos Filho <maasouza@poli.ufrj.br>
2018-07-10 00:01:53 -0300

    Adiciona Graficos

--- src/learn.py
+++ src/learn.py
@@ -13,12 +13,23 @@ import wave
 import os
 from casting import *
 import time as t
+from matplotlib import cm
+#import sklearn as sk
+#import pandas as pd
+#import seaborn as sb
+#import random
+#import itertools
+
+
 
 class SoundObject:
     word = ''
     mfcc = []
     d_mfcc = []
     d2_mfcc = []
+    mfcc_n = []
+    d_mfcc_n = []
+    d2_mfcc_n = []
     logFilterBank = []
     audioSignal = []
     audioFiltered = []
@@ -42,7 +53,9 @@ class SoundObject:
         self.mfcc = lb.feature.mfcc(self.audioFiltered,sr=16000,n_mfcc=13)
         self.d_mfcc = lb.feature.delta(self.mfcc, order=1)
         self.d2_mfcc = lb.feature.delta(self.mfcc, order=2)
-
+        self.mfcc_n = self.mfcc[:]
+        self.d_mfcc_n = self.d_mfcc[:]
+        self;d2_mfcc_n = self.d2_mfcc[:]
         self.mfcc -= (np.mean(self.mfcc, axis=0) + 1e-8)
         self.d_mfcc -= (np.mean(self.d_mfcc, axis=0) + 1e-8)
         self.d2_mfcc -= (np.mean(self.d2_mfcc, axis=0) + 1e-8)        
@@ -68,7 +81,7 @@ class Recoder:
          
         audio = pyaudio.PyAudio()
         os.system('cls' if os.name == 'nt' else 'clear')
-        _ = input("Precione Enter para começar a gravação de ~ 1.5s")
+        _ = input("Pressione Enter para começar a gravação de ~ 1.5s")
 
         # start Recording
         stream = audio.open(format=FORMAT, channels=CHANNELS,
@@ -274,7 +287,8 @@ if __name__ == "__main__":
     }
 
     #inputFile = input("Dataset: ")
-    inputFile = 'train.txt'
+    print("Trainning...")
+    inputFile = 'train_partial.txt'
     with open(inputFile) as file:
         for line in file:
             soundPath, word = line.split()
@@ -285,9 +299,89 @@ if __name__ == "__main__":
             audiosFeatures[word]['d2_mfcc'].append(np.array(audio.d2_mfcc))
             audiosFeatures[word]['logFilterBank'].append(np.array(audio.logFilterBank))
     
+    audio = audios['mais'][2]
+
+    ###########################  PLOT RUIDO SINAL
+    # plt.figure(figsize=(8,6))
+    # plt.subplot(2,1,1)
+    # plt.plot(audio.audioSignal)
+    # plt.subplot(2,1,2)
+    # plt.plot(audio.audioFiltered)
+    # plt.savefig("mais_comparacao_ruido.png")
+    # plt.show()
+
+    ########################## PLOT MFCC
+    # plt.figure(figsize=(9,6))
+    # ax = plt.subplot(3,1,1)
+    # ax.imshow(audio.mfcc,interpolation='nearest', cmap=cm.jet, origin='lower',aspect='auto')
+    # ax.set_title("MFCC")
+
+    # ax = plt.subplot(3,1,2)
+    # ax.imshow(audio.d_mfcc, cmap=cm.jet,interpolation='nearest', origin='lower',aspect='auto')
+    # ax.set_title("Delta MFCC")
+
+    # ax = plt.subplot(3,1,3)
+    # ax.imshow(audio.d2_mfcc, cmap=cm.jet,interpolation='nearest', origin='lower',aspect='auto')
+    # ax.set_title("Delta Delta MFCC")
+    
+    # plt.savefig("mfcc_coef_vs_time.png")
+    # plt.show()
+
+    ################################ PLOT CONFUSION MATRIX
+    # def plot_confusion_matrix(cm, classes,
+    #                       normalize=False,
+    #                       title='Confusion matrix',
+    #                       cmap=cm.jet):
+    #     """
+    #     This function prints and plots the confusion matrix.
+    #     Normalization can be applied by setting `normalize=True`.
+    #     """
+    #     if normalize:
+    #         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    #         print("Normalized confusion matrix")
+    #     else:
+    #         print('Confusion matrix, without normalization')
+
+    #     plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    #     plt.title(title)
+    #     plt.colorbar()
+    #     tick_marks = np.arange(len(classes))
+    #     plt.xticks(tick_marks, classes, rotation=45)
+    #     plt.yticks(tick_marks, classes)
+
+    #     fmt = '.2f' if normalize else 'd'
+    #     thresh = cm.max() / 2.
+    #     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    #         plt.text(j, i, format(cm[i, j], fmt),
+    #                  horizontalalignment="center",
+    #                  color="white" if cm[i, j] > thresh else "black")
+
+    #     plt.tight_layout()
+    #     plt.ylabel('True label')
+    #     plt.xlabel('Predicted label')
+    #     plt.savefig('confusion_matrix.png')
+    #     plt.show()
+    #
+    # print("Testing...")
+    # words = ['zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'mais', 'menos', 'dividido', 'vezes','igual']
+    # testValue = []
+    # correctValue = []
+    # inputFile = 'test_all.txt'
+    # with open(inputFile) as file:
+    #     for line in file:
+    #         soundPath, word = line.split()
+    #         correctValue.append(word)
+    #         audio = SoundObject(soundPath,word)
+    #         resp = compare(audiosFeatures,audio)[0]
+    #         if(resp == None):
+    #             resp = words[random.randint(0,len(words)-1)]
+    #         testValue.append(resp)
+    # cmatrix = sk.metrics.confusion_matrix(correctValue, testValue, labels = words)
+    # plot_confusion_matrix(cmatrix,words)
+
     #a = SoundObject('./data/test/cinco/cinco18.wav')
     #r = Recoder()
     #r.record()
     #a = SoundObject('./data/test/igual/igual20.wav')
     #print(compare(audiosFeatures,a)[0])
-    #mainFunction(audiosFeatures)
+    mainFunction(audiosFeatures)
